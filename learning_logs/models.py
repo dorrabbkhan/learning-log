80e148d90a9daa46a7f92fe003f8998a03306bc4
Dorrabb Khan <dorrabbk@gmail.com>
2019-08-26 21:01:10 +0500

    Initialized topic and entry models, added initial data

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -9,7 +9,24 @@ class Topic(models.Model):
 
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
-    # initialize variables
+    # initialize fields
 
     def __str__(self):
         return self.text
+
+class Entry(models.Model):
+    """
+    Class for user entries
+    """
+
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    # initialize fields
+
+    class Meta:
+        # metadata for an entry
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        return f'{self.text[:50]}...'
