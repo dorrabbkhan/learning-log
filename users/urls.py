654726bd286bd554c5758eaa5d75582965acb2f6
Dorrabb Khan <dorrabbk@gmail.com>
2019-08-28 00:12:23 +0500

    Added functionality to register new users

--- users/urls.py
+++ users/urls.py
@@ -3,10 +3,12 @@ URL patterns for users application
 """
 
 from django.urls import path, include
+from . import views
 
 app_name = 'users'
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls'))
+    path('', include('django.contrib.auth.urls')),
     # default auth urls
+    path('register/', views.register, name='register')
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+def register(request):
+    """
+    Register the user
+    """
+
+    if request.method != "POST":
+        form = UserCreationForm()
+        # make blank form if method is not POST
+
+    else:
+        form = UserCreationForm(data=request.POST)
+        # create the form with user-filled data
+
+        if form.is_valid():
+            new_user = form.save()
+            # if form is valid, save it and create new user
+
+            login(request, new_user)
+            return redirect('learning_logs:index')
+            # log the user in and redirect to home
+    
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
